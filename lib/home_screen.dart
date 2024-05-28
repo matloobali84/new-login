94cd4ab9732510acef7e940536558207ffc267ef
matloobali84 <alimatloob684@gmail.com>
2024-05-28 16:08:36 +0100

    Initial commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,15 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _nameController = TextEditingController();
+
+  void _addData() async {
+    String name = _nameController.text;
+
+    if (name.isNotEmpty) {
+      await FirebaseFirestore.instance.collection('users').add({
+        'name': name,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+
+      _nameController.clear();
+
+      // Show a snackbar on successful data addition
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Data added successfully!')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Screen'),
       ),
-      body: Center(
-        child: Text('Welcome! You are logged in.'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome! You are logged in.',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 40),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Enter your name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton.icon(
+              onPressed: _addData,
+              icon: Icon(Icons.add),
+              label: Text('Add Data'),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                textStyle: TextStyle(fontSize: 16),
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
